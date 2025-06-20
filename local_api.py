ce65a839ba28e160e981853433833553211ea1dc
Matt Canter <mcan128@wgu.edu>
2025-06-19 22:26:54 -0400

    added screenshot of api to screenshots folder, and added the api code to both the main and local_api files

--- local_api.py
+++ local_api.py
@@ -2,15 +2,10 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get("http://127.0.0.1:8000")
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
-
-
+print("GET Status Code:", r.status_code)
+print("GET Result:", r.json())
 
 data = {
     "age": 37,
@@ -29,10 +24,7 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+print("POST Status Code:", r.status_code)
+print("POST Result:", r.json())
